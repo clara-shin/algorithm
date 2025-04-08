b9d67ef765354cc84747c39f9648134321ea8745
Hyejin <hyejin.frontend@gmail.com>
2025-04-08 22:12:58 +0900

    [Silver II] Title: 쇠막대기, Time: 196 ms, Memory: 12892 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.js
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -1,29 +1,23 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
+let answer = 0;
+let stack = [];
+let prevChar = '';
 
-function solution(arrangement) {
-    let answer = 0;
-    let stack = [];
-    let prevChar = '';
-    
-    for (let char of arrangement) {
-        if (char === '(') {
-            stack.push(char);
-        } else { // 닫는 괄호인 경우
-            stack.pop();
-            
-            if (prevChar === '(') {
-                // 레이저인 경우
-                answer += stack.length;
-            } else {
-                // 막대기 끝인 경우
-                answer += 1;
-            }
+for (let char of input) {
+    if (char === '(') {
+        stack.push(char);
+    } else { // 닫는 괄호인 경우
+        stack.pop();
+
+        if (prevChar === '(') {
+            // 레이저인 경우
+            answer += stack.length;
+        } else {
+            // 막대기 끝인 경우
+            answer += 1;
         }
-        prevChar = char;
     }
-    
-    return answer;
+    prevChar = char;
 }
-
-console.log(solution(input));
+console.log(answer);
