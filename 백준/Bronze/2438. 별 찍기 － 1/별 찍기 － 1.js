e39b69cb19de5f13806b71d92d6c7f5e8d92ecc5
Hyejin <devhyejins@gmail.com>
2025-05-13 16:57:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 92 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync('/dev/stdin').toString().split('\n')[0]);
+let result = '';
+for(let i=0; i < n; i++){
+	for(let j=0; j <= i; j++){
+		result += '*';
+	}
+	result += '\n';
+}
+console.log(result);
