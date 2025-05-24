5c023472e2245a50be808e0b64bcc09cf0e40d26
Hyejin <devhyejins@gmail.com>
2025-05-24 12:26:30 +0900

    Time: 41 ms (82.93%), Space: 55.3 MB (44.27%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.js
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.js
@@ -1,19 +1,5 @@
-/**
- * @param {number} n - a positive integer
- * @return {number} - a positive integer
- */
-var reverseBits = function(n) {
-    let result = 0;
-    
-    for (let i = 0; i < 32; i++) {
-        // 결과에 현재 비트를 추가 (비트 OR 연산)
-        // 결과를 왼쪽으로 시프트한 후, n의 최하위 비트가 1이면 결과에 1을 더함
-        result = (result << 1) | (n & 1);
-        
-        // n을 오른쪽으로 시프트
-        n >>>= 1;
-    }
-    
-    // 부호 없는 정수로 변환
-    return result >>> 0;
+function reverseBits(n) {
+    const binaryStr = n.toString(2).padStart(32, '0')
+    const reverseNum = binaryStr.split('').reverse().join('')
+    return parseInt(reverseNum, 2)
 };
